cd7a70e6fed08dd041b42841aa80ff7752e3924c
codeboyfriend <mkinggrafix@gmail.com>
2022-10-09 14:32:01 -0700

    Add Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import Header from "./components/Header";
 import Board from "./components/Board";
 import Map from "./components/Map";
 import { useEffect, useState } from "react";
-import BG from "./assets/images/pattern-bg.png";
 
 function App() {
   const [address, setAddress] = useState(null);
   const [ipAddress, setIpAddress] = useState();
   const checkIpAddress = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
   const checkDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[-zA-Z]{2,})+/
-  // console.log(import.meta.env.VITE_APP_API_KEY)
 
   const fetchData = () => {
     try {
       const getData = async () => {
-        const getInitialData = await fetch(`https:geo.ipify.org/api/v2/country,city?apiKey=at_ns6JztMHXCUkpwqabV9lyXmVVuRFm&${checkIpAddress.test(ipAddress) ? `ipAddress=${ipAddress}` : checkDomain.test(ipAddress) ? `domain=${ipAddress}` : ""
+        const getInitialData = await fetch(`https:geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_APP_API_KEY}&${checkIpAddress.test(ipAddress) ? `ipAddress=${ipAddress}` : checkDomain.test(ipAddress) ? `domain=${ipAddress}` : ""
       }`)
         const data = await getInitialData.json()
         setAddress(data)
@@ -47,7 +45,11 @@ function App() {
   ) : (
     <div className="loading h-[100vh] flex flex-col justify-center items-center">
       <h2 className="text-2xl font-bold mb-2">IP ADDRESS TRACKER</h2>
-      <p>Please wait</p>
+      <p>Please wait 
+        <span className="loadOne">.</span>
+        <span className="loadTwo">.</span>
+        <span className="loadThree">.</span>
+      </p>
     </div>
   )
 }
